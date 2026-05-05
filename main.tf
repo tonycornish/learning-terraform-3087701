879545f5b20fad256c96656ea080b1daf341eb62
tonycornish <116107427+tonycornish@users.noreply.github.com>
2026-05-05 11:54:07 -0400

    Update security group ingress and egress CIDR blocks

--- main.tf
+++ main.tf
@@ -36,11 +36,11 @@ name = "blog_new"
 
 vpc_id = data.aws_vpc.default.id
 
-ingress_rules = ["http-80-tcp","https-443-tcp"]
-ingress_cidr  = ["0.0.0.0/0"]
+ingress_rules        = ["http-80-tcp","https-443-tcp"]
+ingress_cidr_blocks  = ["0.0.0.0/0"]
 
-egress_rules = [all-all]
-egress_cidr  = ["0.0.0.0/0"]
+egress_rules        = ["all-all"]
+egress_cidr_blocks  = ["0.0.0.0/0"]
 
 }
 
