2be5bfc6fd0426c8996195af9f5312ef6d7158a6
tonycornish <116107427+tonycornish@users.noreply.github.com>
2026-05-06 11:09:22 -0400

    web to blog

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
