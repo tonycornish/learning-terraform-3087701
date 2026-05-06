aa774217ab5d10a6d8c11d3c6eff08cf39f0c14b
tonycornish <116107427+tonycornish@users.noreply.github.com>
2026-05-06 10:55:33 -0400

    Create a new dev vpc

--- main.tf
+++ main.tf
@@ -14,15 +14,31 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "uc-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-vpc_security_group_ids = [module.blog_sg.security_group_id]
+subnet_id = module.blog_vpc.public_subnets [0]
 
   tags = {
     Name = "HelloWorld"
@@ -32,9 +48,9 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
 source  = "terraform-aws-modules/security-group/aws"
 version = "5.3.1"
-name = "blog"
 
-vpc_id = data.aws_vpc.default.id
+vpc_id  = module.blog_vpc.vpc_id
+name    = "blog"
 
 ingress_rules        = ["http-80-tcp","https-443-tcp"]
 ingress_cidr_blocks  = ["0.0.0.0/0"]
