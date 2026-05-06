5ad4f2d871ca5602b3fc31811b97952bae25a33a
tonycornish <116107427+tonycornish@users.noreply.github.com>
2026-05-06 11:24:07 -0400

    autoscaling

--- main.tf
+++ main.tf
@@ -33,20 +33,6 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-subnet_id = module.blog_vpc.public_subnets [0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-
-
 module "blog_sg" {
 source  = "terraform-aws-modules/security-group/aws"
 version = "5.3.1"
@@ -97,4 +83,27 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_group_arn = aws_lb_target_group.blog.arn
   target_id        = aws_instance.blog.id
   port             = 80
+}
+
+module "blog_autoscaling" {
+source  = "terraform-aws-modules/autoscaling/aws"
+version = "9.2.1"
+
+name = "blog"
+
+min_size = 1
+max_size = 2
+
+vpc_zone_identifier = module.blog_vpc.public_subnets
+
+launch_template_name = "blog"
+security_groups      = [module.blog_sg.vpc_security_group_id]
+instance_type        = var.instance_type 
+image_id             = data.aws_ami.app_ami.id
+
+traffic_source_attachments = {
+  blog_alb = {
+    traffic_source_identifier = aws_lb_target_group.blog.arn
+  }
+ }
 }
